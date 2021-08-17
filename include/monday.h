4e500448ae2097dbe8372d2f7ec032e76899c50f
Augustine129 <74011126+Augustine129@users.noreply.github.com>
2021-08-17 19:04:32 +0000

    Add Kelvin

--- include/monday.h
+++ include/monday.h
@@ -1,3 +1,38 @@
+#include <iostream>
+
+float f_to_c(float f) {
+  return ((f - 32) * 5) / 9; //Fahrenheit to Centigrade.
+}
+float c_to_f(float c) {
+  return ((c * 9) / 5) + 32; //Centigrade to Fahrenheit.
+}
+
+float c_to_k(float c) {
+  return (c + 273.15); //Centigrade to Kelvin.
+}
+
+float k_to_c(float k) {
+  return (k - 273.15); // Kelvin to Centigrade.
+}
+
+float f_to_k(float f) {
+  return ((f + 459.67) * 5 / 9); //Fahrenheit to Kelvin.
+}
+
+float k_to_f(float k) {
+  return ((k * 9 / 5) - 459.67); //Kelvin to Fahrenheit.
+}
+
+void sub_menu(){
+  //menu options
+  std::cout << "\nPress ‘A’ to convert from Fahrenheit to Centigrade.";
+  std::cout << "\nPress ‘B’ to convert from Centigrade to Fahrenheit.";
+  std::cout << "\nPress ‘C’ to convert from Centigrade to Kelvin.";
+  std::cout << "\nPress ‘D’ to convert from Kelvin to Centigrade.";
+  std::cout << "\nPress ‘E’ to convert Fahrenheit to Kelvin.";
+  std::cout << "\nPress ‘F’ to convert Kelvin to Fahrenheit.";
+}
+
 void fahrenheitCentigradeConversion(void) {
   
   std::string temp;
