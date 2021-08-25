f14182aa8a650ee4e06ed89c873fdfa15f6ac538
Augustine129 <74011126+Augustine129@users.noreply.github.com>
2021-08-25 00:39:04 +0000

    Primer 9 First attempt

--- include/wednesday.h
+++
@@ -1,8 +0,0 @@
-void phoneDirectory(void) {
-	std::cout << " - phoneDirectory: not yet implemented\n\n";
-}
-
-
-void dataFileParser(void) {
-	std::cout << " - dataFileParser: not yet implemented\n\n";
-}
